60b75f4384790bedd22ae48462a784c592798b52
KoriKosmos <KoriKosmos@gmail.com>
2023-08-11 17:06:03 +0100

    added random Shuba background

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('DOMContentLoaded', async () => {
     const usernameElements = document.querySelectorAll('.username');
     const username = 'KoriKosmos';
 
@@ -12,4 +12,28 @@ document.addEventListener('DOMContentLoaded', () => {
     const copyrightElement = document.querySelector('#copyright');
     const currentYear = new Date().getFullYear();
     copyrightElement.textContent = `${currentYear} ${username}`;
+
+    // New code to fetch a random artwork and credit the artist
+    const backgroundImageElement = document.body; // Select the body or a specific element as the background
+
+    // Fetch a random artwork of "Oozora Subaru" using the Donmai API
+    const apiUrl = 'https://danbooru.donmai.us/posts.json?tags=oozora_subaru';
+    try {
+        const response = await fetch(apiUrl);
+        const artworks = await response.json();
+
+        // Choose a random artwork from the fetched data
+        const randomIndex = Math.floor(Math.random() * artworks.length);
+        const randomArtwork = artworks[randomIndex];
+
+        // Set the chosen artwork as the background image
+        backgroundImageElement.style.backgroundImage = `url(${randomArtwork.file_url})`;
+
+        // Credit the artist in the footer
+        const artistName = randomArtwork.tag_string_artist;
+        const footerArtistElement = document.querySelector('.footer-artist');
+        footerArtistElement.textContent = `Artwork by ${artistName}`;
+    } catch (error) {
+        console.error('Error fetching artwork:', error);
+    }
 });
