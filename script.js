220abe23c312efc54d90b48f7a64c17607955053
KoriKosmos <KoriKosmos@gmail.com>
2023-08-11 15:25:50 +0100

    barebones

---
+++ script.js
@@ -0,0 +1,14 @@
+// script.js
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    const usernameElements = document.querySelectorAll('#username');
+    const username = 'KoriKosmos';
+
+    usernameElements.forEach(element => {
+        element.textContent = username;
+    });
+
+    const copyrightElement = document.querySelector('#copyright');
+    const currentYear = new Date().getFullYear();
+    copyrightElement.textContent = `${currentYear}`;
+});
