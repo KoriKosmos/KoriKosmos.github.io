17255de2153485e033b6c244cc28951631d436df
KoriKosmos <KoriKosmos@gmail.com>
2023-08-11 17:53:18 +0100

    hopefully some CSS fixes part 2 loles

--- script.js
+++ script.js
@@ -17,7 +17,12 @@ document.addEventListener('DOMContentLoaded', async () => {
     const backgroundImageElement = document.body; // Select the body or a specific element as the background
 
     // Fetch a random artwork of "Oozora Subaru" using the Donmai API
-    const apiUrl = 'https://danbooru.donmai.us/posts.json?tags=oozora_subaru+-rating%3Aexplicit+-rating%3Asuggestive';
+
+    // Choose either "highres" or "absurdres" tag
+    const chosenTag = Math.random() < 0.5 ? 'highres' : 'absurdres';
+
+    // Fetch images with the chosen tag
+    const apiUrl = `https://danbooru.donmai.us/posts.json?tags=oozora_subaru+-rating%3Aexplicit+-rating%3Asuggestive+${chosenTag}`;
     try {
         const response = await fetch(apiUrl);
         const artworks = await response.json();
@@ -25,6 +30,29 @@ document.addEventListener('DOMContentLoaded', async () => {
         // Choose a random artwork from the fetched data
         const randomIndex = Math.floor(Math.random() * artworks.length);
         const randomArtwork = artworks[randomIndex];
+
+
+        // Calculate screen dimensions
+        const screenWidth = window.innerWidth;
+        const screenHeight = window.innerHeight;
+
+        // Calculate the aspect ratio of the fetched image
+        const aspectRatio = randomArtwork.image_width / randomArtwork.image_height;
+
+        // Calculate appropriate background dimensions based on screen size
+        let backgroundWidth, backgroundHeight;
+        if (screenWidth / screenHeight > aspectRatio) {
+            // Screen is wider than the image's aspect ratio
+            backgroundWidth = screenWidth;
+            backgroundHeight = screenWidth / aspectRatio;
+        } else {
+            // Screen is taller than the image's aspect ratio
+            backgroundHeight = screenHeight;
+            backgroundWidth = screenHeight * aspectRatio;
+        }
+
+        // Set the background size
+        backgroundImageElement.style.backgroundSize = `${backgroundWidth}px ${backgroundHeight}px`;
 
         // Set the chosen artwork as the background image
         backgroundImageElement.style.backgroundImage = `url(${randomArtwork.file_url})`;
