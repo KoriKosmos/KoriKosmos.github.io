3db35a0b6cd5a1c58f8bc944898348438caebf1f
KoriKosmos <90063618+KoriKosmos@users.noreply.github.com>
2024-05-14 12:13:22 +0100

    Add logging to fetch-tracks.mjs

--- fetch-tracks.mjs
+++ fetch-tracks.mjs
@@ -8,25 +8,30 @@ const username = process.env.LASTFM_USERNAME;
 fetch(`https://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=${username}&api_key=${apiKey}&format=json`)
     .then(response => response.json())
     .then(data => {
-        const tracks = data.recenttracks.track;
-        let filteredTracks = [];
+        console.log('API Response:', JSON.stringify(data, null, 2)); // Log the entire API response
+        if (data && data.recenttracks && data.recenttracks.track) {
+            const tracks = data.recenttracks.track;
+            let filteredTracks = [];
 
-        for (let i = 0; i < tracks.length; i++) {
-            const track = tracks[i];
-            const isMagTrack = /\bMAG\b/.test(track.name);
-            const isSurahTrack = /\bSurah\b/.test(track.name);
+            for (let i = 0; i < tracks.length; i++) {
+                const track = tracks[i];
+                const isMagTrack = /\bMAG\b/.test(track.name);
+                const isSurahTrack = /\bSurah\b/.test(track.name);
 
-            if ((i === 0 || track.name !== tracks[i - 1].name) && !isMagTrack && !isSurahTrack) {
-                filteredTracks.push(track);
+                if ((i === 0 || track.name !== tracks[i - 1].name) && !isMagTrack && !isSurahTrack) {
+                    filteredTracks.push(track);
+                }
             }
+
+            let trackList = '<ul>';
+            filteredTracks.forEach(track => {
+                trackList += `<li>${track.artist['#text']} - ${track.name}</li>`;
+            });
+            trackList += '</ul>';
+
+            fs.writeFileSync('./_includes/latest-tracks.html', trackList);
+        } else {
+            console.error('Invalid response structure:', data);
         }
-
-        let trackList = '<ul>';
-        filteredTracks.forEach(track => {
-            trackList += `<li>${track.artist['#text']} - ${track.name}</li>`;
-        });
-        trackList += '</ul>';
-
-        fs.writeFileSync('./_includes/latest-tracks.html', trackList);
     })
     .catch(error => console.error('Error fetching data from Last.fm API:', error));
